997a032226d6c342c90b7297d9ae566ae4f863bd
yoggie13 <ognjennikolic13@gmail.com>
2022-03-02 17:17:34 +0100

    api changes

--- src/services/api/API.js
+++ src/services/api/API.js
@@ -1,4 +1,4 @@
-const url_main = "https://teorijaigara.azurewebsites.net/";
+const url_main = "https://teorijaigara.azurewebsites.net/api/";
 const headers_main = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Credentials': true,
