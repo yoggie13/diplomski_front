fab19c13dd70db28c637e8ba16b77af79398eeb8
yoggie13 <ognjennikolic13@gmail.com>
2022-05-07 11:53:33 +0200

    second batch of changes after test phase

--- src/components/admin/AllGames.js
+++ src/components/admin/AllGames.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import Loading from '../Loading';
 import GameServices from '../../services/GameServices.js'
+import { useHistory } from 'react-router-dom';
 
 export default function AllGames() {
 
     const [gamesState, setGamesState] = useState();
     const [loadingState, setLoadingState] = useState(true);
+    let history = useHistory()
 
     useEffect(() => {
         document.title = "Sve igre | Teorija igara"
@@ -40,36 +41,25 @@ export default function AllGames() {
             : gamesState !== false
                 ? <div className="AllGames">
                     <h1>Pregled igara</h1>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Naziv igre</th>
-                                <th>Broj igrača koji su odigrali</th>
-                                <th>Aktivna</th>
-                                <th>Više info</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                gamesState.map((game) =>
-                                    <tr key={game.id}>
-                                        <td>{game.name}</td>
-                                        <td>{game.playersPlayed}</td>
-                                        <td id="center">{
-                                            game.active
-                                                ? <i className="fas fa-check-circle" id="icon-true"></i>
-                                                : <i className="fas fa-times-circle" id="icon-false"></i>
-                                        }
-                                        </td>
-                                        <td id="center">
-                                            <Link to={`dashboard/${game.id}`}>
-                                                <i id={game.id} className="fas fa-chevron-right"></i>
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                )}
-                        </tbody>
-                    </table>
+                    <div className='GamesArray'>
+                        {
+                            gamesState.map((game) =>
+                                <div key={game.id} className='GameInfo'
+                                    onClick={e => {
+                                        e.preventDefault()
+                                        history.pushState(`/game/${game.id}`)
+                                    }}>
+                                    <h3>{game.name}</h3>
+                                    <p>Оdigralo: {game.playersPlayed}</p>
+                                    <p>Aktivna: {
+                                        game.active
+                                            ? <i className="fas fa-check-circle" id="icon-true"></i>
+                                            : <i className="fas fa-times-circle" id="icon-false"></i>
+                                    }
+                                    </p>
+                                </div>
+                            )}
+                    </div>
                 </div>
                 : <p>Trenutno nema aktivnih igara</p>
     )
