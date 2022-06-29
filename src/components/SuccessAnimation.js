f559fe782c17ee5d42f717bbea54260ff0708296
yoggie13 <ognjennikolic13@gmail.com>
2022-06-29 12:00:29 +0200

    separated Success css

--- src/components/SuccessAnimation.js
+++ src/components/SuccessAnimation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import successGif from '../assets/successv3.gif'
+import '../assets/styles/Success.css'
 
 export default function SuccessAnimation({ setSuccessState }) {
 
